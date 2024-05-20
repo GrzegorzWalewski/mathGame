fd3099feb06865548a82cf32652886601342b5ae
Grzojda <grzojda@gmail.com>
2024-05-20 15:27:28 +0200

    added livewire, tailwind, removed dashboard

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,17 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
+    server: {
+        port: 5173,
+        strictPort: true,
+        host: true,
+        origin: 'http://localhost:5173',
+    }
 });
